119f68be78f45956b0ebab3c08fb3e5651896358
Thuc Le <lethuc.crma@gmail.com>
2022-07-05 15:49:52 +0700

    feat: complete add token screen

--- src/screens/AddTokenScreen.js
+++ src/screens/AddTokenScreen.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView, StyleSheet, Text, FlatList,
+  SafeAreaView, StyleSheet, FlatList, Alert,
 } from 'react-native';
+import { Dialog, Button, Paragraph } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+import APIs from '../apis';
 import CoinCard from '../components/CoinCard';
+import server from '../configs/server';
 import symbols from '../constants/symbols';
+import WalletSlice from '../redux/wallet/wallet.slice';
+import { generateURL } from '../utils/string';
 
 const styles = StyleSheet.create({
   container: {
@@ -21,7 +27,46 @@ const styles = StyleSheet.create({
 const tokenKeyExtractor = ({ code }) => code;
 
 function AddTokenScreen() {
-  const renderCoinCard = ({ item }) => <CoinCard code={item.code} value={item.value} />;
+  const [selectedToken, setToken] = useState(null);
+  const [requesting, setRequesting] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleSelectToken = (selected) => setToken(selected);
+  const hideDialog = () => setToken(null);
+
+  const renderCoinCard = ({ item }) => (
+    <CoinCard onPress={handleSelectToken} code={item.code} value={item.value} />
+  );
+
+  const handleAddToken = async () => {
+    setRequesting(true);
+    try {
+      await APIs.post(generateURL(server.ADD_TOKEN, { walletId: 1 }), {
+        symbol: selectedToken.code,
+        name: selectedToken.value,
+      });
+
+      // refetch wallet
+      dispatch(WalletSlice.actions.fetchWallet());
+
+      Alert.alert('Token was added successfully');
+    } catch (err) {
+      Alert.alert('Something went wrong :(');
+      console.log(err);
+    }
+    hideDialog();
+    setRequesting(false);
+  };
+
+  const genInfo = () => {
+    if (requesting) {
+      return 'Processing... Please wait!';
+    }
+    return selectedToken
+      ? `Do you want to add token ${selectedToken.code}-${selectedToken.value}`
+      : '';
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -32,6 +77,15 @@ function AddTokenScreen() {
         keyExtractor={tokenKeyExtractor}
         renderItem={renderCoinCard}
       />
+      <Dialog visible={!!selectedToken} onDismiss={hideDialog}>
+        <Dialog.Content>
+          <Paragraph>{genInfo()}</Paragraph>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button disabled={requesting} onPress={hideDialog}>Cancel</Button>
+          <Button disabled={requesting} onPress={handleAddToken}>Ok</Button>
+        </Dialog.Actions>
+      </Dialog>
     </SafeAreaView>
   );
 }
