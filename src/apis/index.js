ce9781d025c5f47c4d9089965b78e029e05678d2
Thuc Le <lethuc.crma@gmail.com>
2022-07-05 15:48:49 +0700

    feat: add api interceptors

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,32 @@
 import axios from 'axios';
 import server from '../configs/server';
+import AuthSlice from '../redux/auth/auth.slice';
+import { store } from '../redux/store';
 
-export default axios.create({
+const APIs = axios.create({
   baseURL: server.ROOT_ENDPOINT,
   headers: {
     Accept: 'application/json',
   },
 });
+
+const responseHandler = (response) => response;
+
+const requestHandler = (request) => {
+  request.headers.Authorization = `Bearer ${store.getState().auth.token}`;
+  return request;
+};
+
+const errorHandler = (error) => {
+  if (error.response && error.response.status === 401) {
+    // token is invalid
+    store.dispatch(AuthSlice.actions.logout());
+  }
+  return Promise.reject(error);
+};
+
+APIs.interceptors.response.use(responseHandler, errorHandler);
+
+APIs.interceptors.request.use(requestHandler, errorHandler);
+
+export default APIs;
